a445ee71a3f319cb40daf488941e927a9da3e3ca
Samuel-Abusa <samuelabusakennedy@gmail.com>
2021-04-18 21:16:15 +0300

    made a toggle-like function on the elements of what we do section

--- studio.js
+++ studio.js
@@ -22,3 +22,24 @@ $ (document).ready(function(){
     $ ('.jImg3').css({'display' : 'none'})
   })
 });
+
+$ (document).ready(function(){
+  $ ('.jPrg1').click(function(){
+    $ ('.jImg1').css({'display' : 'block'})
+    $ ('.jPrg1').css({'display' : 'none'})
+  })
+});
+
+$ (document).ready(function(){
+  $ ('.jPrg2').click(function(){
+    $ ('.jImg2').css({'display' : 'block'})
+    $ ('.jPrg2').css({'display' : 'none'})
+  })
+});
+
+$ (document).ready(function(){
+  $ ('.jPrg3').click(function(){
+    $ ('.jImg3').css({'display' : 'block'})
+    $ ('.jPrg3').css({'display' : 'none'})
+  })
+});
